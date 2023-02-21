230338affbf5b2c9d0b490d57e38d47a1f497d90
kitsunereyna <begimbetova.dana@gmail.com>
2023-02-21 19:49:22 +0600

    base url changed

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,5 @@
 import { QueryClient } from "react-query";
 
-// export const BASE_URL =  "https://gutsy-stove-production.up.railway.app"
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL =  "https://gutsy-stove-production.up.railway.app"
 
 export const queryClient = new QueryClient();
